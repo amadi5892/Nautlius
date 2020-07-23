c4ad0ddf1ab46172799a37a357a5d7712d4ca5e5
amadi5892 <66728197+amadi5892@users.noreply.github.com>
2020-07-23 10:21:38 -0400

    added jumbotron

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Header() {
+
+
+    return(
+        <div>
+            <div className="jumbotron jumbotron-fluid paralsec">
+                <div className="container">
+                    <p className="lead">THE NAUTLIUS SPOTLIGHT</p>
+                    <h1 className="display-4">WHY THE LAWS OF PHYSICS ARE INEVITABLE</h1>
+                    <button className="jumbo-btn">READ NOW</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
